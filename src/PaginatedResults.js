d98aaf11d7dc436e740b43512787e2fecf5ac50a
sonu28shamra99 <sonu28shamra99@gmail.com>
2021-08-30 12:30:40 +0530

    All minor bugs fixed

--- src/PaginatedResults.js
+++ src/PaginatedResults.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import { Grid } from '@material-ui/core';
 
